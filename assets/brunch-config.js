6e04829d051f19d51029eacc07fb131343038c6d
Eric Oestrich <eric@oestrich.org>
2018-06-26 20:31:38 -0400

    Set up basic bootstrap

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -2,22 +2,12 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
+      joinTo: "js/app.js",
+      order: {
+        before: [
+          /jquery/,
+        ]
+      }
     },
     stylesheets: {
       joinTo: "css/app.css"
