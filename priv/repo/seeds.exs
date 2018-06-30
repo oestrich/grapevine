84902cc33aa6be0e5786118398134ac4e08cd09f
Eric Oestrich <eric@oestrich.org>
2018-06-30 14:53:02 -0400

    Update seeds to include a default channel

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,3 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Gossip.Repo.insert!(%Gossip.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Gossip.Channels
+
+{:ok, _channel} = Channels.create(%{name: "gossip"})
