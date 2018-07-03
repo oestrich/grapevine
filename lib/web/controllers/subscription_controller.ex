192865fb0cc65936b175fcad8768312270814d70
Eric Oestrich <eric@oestrich.org>
2018-07-02 22:38:46 -0400

    Push finding the game deeper into channels

--- lib/web/controllers/subscription_controller.ex
+++ lib/web/controllers/subscription_controller.ex
@@ -8,13 +8,17 @@ defmodule Web.SubscriptionController do
   def update(conn, %{"id" => id, "game" => %{"subscriptions" => channel_ids}}) do
     %{current_user: user} = conn.assigns
 
-    game = Enum.find(user.games, &(to_string(&1.id) == id))
     channel_ids = Enum.reject(channel_ids, &(&1 == ""))
 
-    case Channels.subscribe_to_channels(game, channel_ids) do
+    case Channels.subscribe_to_channels(user, id, channel_ids) do
       {:ok, _game} ->
         conn
         |> put_flash(:info, "Subscriptions updated!")
+        |> redirect(to: game_path(conn, :index))
+
+      :error ->
+        conn
+        |> put_flash(:info, "Could not update subscriptions.")
         |> redirect(to: game_path(conn, :index))
     end
   end
--- test/gossip/channels_test.exs
+++ test/gossip/channels_test.exs
@@ -9,29 +9,35 @@ defmodule Gossip.ChannelsTest do
       game = create_game(user)
       channel = create_channel()
 
-      %{game: game, channel: channel}
+      user = %{user | games: [game]}
+
+      %{user: user, game: game, channel: channel}
     end
 
-    test "creates subscribed channels", %{game: game, channel: channel} do
-      {:ok, game} = Channels.subscribe_to_channels(game, [channel.id])
+    test "creates subscribed channels", %{user: user, game: game, channel: channel} do
+      {:ok, game} = Channels.subscribe_to_channels(user, game.id, [channel.id])
 
       assert length(game.subscribed_channels) == 1
     end
 
-    test "removes old subscribed channels", %{game: game, channel: channel} do
-      {:ok, game} = Channels.subscribe_to_channels(game, [channel.id])
-      {:ok, game} = Channels.subscribe_to_channels(game, [])
+    test "removes old subscribed channels", %{user: user, game: game, channel: channel} do
+      {:ok, game} = Channels.subscribe_to_channels(user, game.id, [channel.id])
+      {:ok, game} = Channels.subscribe_to_channels(user, game.id, [])
 
       assert length(game.subscribed_channels) == 0
     end
 
-    test "subscribes and removes all in one go", %{game: game, channel: channel} do
+    test "subscribes and removes all in one go", %{user: user, game: game, channel: channel} do
       new_channel = create_channel(%{name: "general"})
 
-      {:ok, game} = Channels.subscribe_to_channels(game, [channel.id])
-      {:ok, game} = Channels.subscribe_to_channels(game, [new_channel.id])
+      {:ok, game} = Channels.subscribe_to_channels(user, game.id, [channel.id])
+      {:ok, game} = Channels.subscribe_to_channels(user, game.id, [new_channel.id])
 
       assert length(game.subscribed_channels) == 1
+    end
+
+    test "game id not found", %{user: user, channel: channel} do
+      assert :error = Channels.subscribe_to_channels(user, "-1", [channel.id])
     end
   end
 end
