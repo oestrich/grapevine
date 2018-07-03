68ca0be110a70fdfc34f72b2e49dcbbe301fa5fd
Eric Oestrich <eric@oestrich.org>
2018-07-02 23:22:30 -0400

    Display a connected message and scroll to the bottom of the chat pane
    On new messages

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -12,19 +12,21 @@ export default class ChatSocket {
     this.socket.connect();
 
     this.channel = this.socket.channel(`channels:${this.channelName}`, {});
-    this.channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) });
+    this.channel.join().receive("ok", resp => { this.append("Connected") });
 
     this.channel.on("messages/broadcast", data => {
       let message = `<span class="blue">${data.name}@${data.game}</span> says, <span class="green">"${data.message}"</span><br/>`;
-
-      let fragment = document.createDocumentFragment();
-      let span = document.createElement('span');
-      span.innerHTML = message;
-      fragment.appendChild(span);
-
-      this.channelElement.appendChild(fragment);
+      this.append(message);
     });
+  }
+
+  append(message) {
+    let fragment = document.createDocumentFragment();
+    let span = document.createElement('span');
+    span.innerHTML = message;
+    fragment.appendChild(span);
+
+    this.channelElement.appendChild(fragment);
+    this.channelElement.scrollTop = this.channelElement.scrollHeight;
   }
 }
