26cfcf0db4be52ca885c94991080aebffc90f248
Eric Oestrich <eric@oestrich.org>
2018-07-08 18:31:20 -0400

    Set version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gossip.Mixfile do
   def project do
     [
       app: :gossip,
-      version: "0.0.1",
+      version: "1.0.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
