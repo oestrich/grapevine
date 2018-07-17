37686c999de9eb58ecd1dc0af1b4a72bdce9dc12
Eric Oestrich <eric@oestrich.org>
2018-07-16 22:34:23 -0400

    Remove username/password from configs

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :gossip, Gossip.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "gossip_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :gossip, Gossip.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "gossip_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
