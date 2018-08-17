129c977bdc6c0c37a37c1455560ab9d7ac2ced7b
Eric Oestrich <eric@oestrich.org>
2018-08-17 19:06:57 -0400

    Display a highlighted game on the front page of gossip

--- lib/web/controllers/page_controller.ex
+++ lib/web/controllers/page_controller.ex
@@ -1,8 +1,15 @@
 defmodule Web.PageController do
   use Web, :controller
 
+  alias Gossip.Presence
+  alias Web.Game
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    game = Game.highlighted_game(Presence.online_games())
+
+    conn
+    |> assign(:game, game)
+    |> render("index.html")
   end
 
   def docs(conn, _params) do
---
+++ lib/web/game.ex
@@ -0,0 +1,17 @@
+defmodule Web.Game do
+  @moduledoc """
+  Helpers for games in the web view
+  """
+
+  @doc """
+  Chose a random game that is online and has a home page
+  """
+  @spec highlighted_game([Game.t()]) :: Game.t()
+  def highlighted_game(games) do
+    games
+    |> Enum.map(& &1.game)
+    |> Enum.filter(&(&1.homepage_url != nil))
+    |> Enum.shuffle()
+    |> List.first()
+  end
+end
