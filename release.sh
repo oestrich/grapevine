37316bc54ba186e1d8e65b66167c118bbedd0a8c
Eric Oestrich <eric@oestrich.org>
2018-08-24 20:23:16 -0400

    Something is broken with the brunch production build

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf priv/static/*
-cd assets && node node_modules/brunch/bin/brunch build -p && cd ..
+cd assets && node node_modules/brunch/bin/brunch build && cd ..
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release
 rm -r priv/static/*
