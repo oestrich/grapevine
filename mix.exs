2537f7a2463808065a47b0699d7ea77b790f7882
Eric Oestrich <eric@oestrich.org>
2018-10-08 18:33:36 -0400

    Start documenting a new event, `games/status`
    This bumps the gossip version, to 2.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gossip.Mixfile do
   def project do
     [
       app: :gossip,
-      version: "2.0.0",
+      version: "2.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
