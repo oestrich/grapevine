4dd9f2611f0ebe568ebb82be9d2f3f4d30b70a0a
Eric Oestrich <eric@oestrich.org>
2018-12-15 18:04:54 -0500

    Move to loading during application

--- lib/web/controllers/game_statistic_controller.ex
+++ lib/web/controllers/game_statistic_controller.ex
@@ -4,14 +4,15 @@ defmodule Web.GameStatisticController do
   alias Gossip.Games
   alias Gossip.Statistics
 
-  @grapevine_url Application.get_env(:gossip, :grapevine)[:cors_host]
 
   def players(conn, %{"game_id" => short_name}) do
+    grapevine_url = Application.get_env(:gossip, :grapevine)[:cors_host]
+
     case Games.get_by_short(short_name) do
       {:ok, game} ->
         conn
         |> assign(:statistics, Statistics.last_few_days(game))
-        |> put_resp_header("access-control-allow-origin", @grapevine_url)
+        |> put_resp_header("access-control-allow-origin", grapevine_url)
         |> render("players.json")
 
       {:error, :not_found} ->
