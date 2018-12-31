9a1b4b7afaa319706b5ca5151d6442a3a642aa3a
Eric Oestrich <eric@oestrich.org>
2018-12-30 19:39:16 -0500

    Start on an MSSP client to track player counts

---
+++ lib/gossip/telnet/client.ex
@@ -0,0 +1,195 @@
+defmodule Gossip.Telnet.Client do
+  @moduledoc """
+  A client to check for MSSP data
+  """
+
+  use GenServer
+
+  alias Gossip.Telnet.Client.Options
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts)
+  end
+
+  defp send_term_type() do
+    GenServer.cast(self(), {:send_term_type})
+  end
+
+  defp record_mssp() do
+    GenServer.cast(self(), {:record_mssp})
+  end
+
+  def init(opts) do
+    Process.send_after(self(), {:stop}, 15_000)
+
+    {:ok, %{active: false, host: Keyword.get(opts, :host), port: Keyword.get(opts, :port)}, {:continue, :connect}}
+  end
+
+  def handle_continue(:connect, state) do
+    {:ok, socket} = :gen_tcp.connect(String.to_charlist(state.host), state.port, [:binary, {:packet, 0}])
+    {:noreply, Map.put(state, :socket, socket)}
+  end
+
+  def handle_cast({:record_mssp}, state) do
+    :gen_tcp.send(state.socket, <<255, 253, 70>>)
+    {:noreply, state}
+  end
+
+  def handle_cast({:send_term_type}, state) do
+    :gen_tcp.send(state.socket, <<255, 251, 24>>)
+    :gen_tcp.send(state.socket, <<255, 250, 24, 0>> <> "Mudlet" <> <<255, 240>>)
+    {:noreply, state}
+  end
+
+  def handle_info({:stop}, state) do
+    IO.inspect "terminating due to no mssp data"
+    {:stop, :normal, state}
+  end
+
+  def handle_info({:tcp, _port, data}, state) do
+    IO.inspect data
+    IO.inspect Options.parse(data)
+
+    cond do
+      Options.supports_mssp?(data) ->
+        record_mssp()
+        {:noreply, state}
+
+      Options.mssp_data?(data) ->
+        IO.inspect "got mssp data"
+
+        data
+        |> Options.parse()
+        |> Enum.map(&Options.parse_mssp/1)
+        |> IO.inspect()
+
+        {:stop, :normal, state}
+
+      Options.term_type_request?(data) ->
+        IO.inspect "term type requested"
+        send_term_type()
+
+        {:noreply, state}
+
+      true ->
+        {:noreply, state}
+    end
+  end
+
+  defmodule Options do
+    @moduledoc """
+    Parse telnet IAC options coming from the game
+    """
+
+    def supports_mssp?(binary) do
+      binary
+      |> parse()
+      |> Enum.member?([255, 251, 70])
+    end
+
+    def term_type_request?(binary) do
+      binary
+      |> parse()
+      |> Enum.member?([255, 253, 24])
+    end
+
+    def mssp_data?(binary) do
+      options = parse(binary)
+      Enum.any?(options, fn option ->
+        match?([255, 250, 70 | _], option)
+      end)
+    end
+
+    def parse(binary) do
+      binary
+      |> options([], [])
+      |> Enum.reverse()
+      |> Enum.filter(&(List.first(&1) == 255))
+    end
+
+    def options(<<>>, current, stack) do
+      [Enum.reverse(current) | stack]
+    end
+
+    def options(<<255, 250, data :: binary>>, current, stack) do
+      {sub, data} = parse_sub_negotiation(<<255, 250>> <> data)
+      options(data, [], [sub, Enum.reverse(current) | stack])
+    end
+
+    def options(<<255, 251, byte :: size(8), data :: binary>>, current, stack) do
+      options(data, [], [[255, 251, byte], Enum.reverse(current) | stack])
+    end
+
+    def options(<<255, 253, byte :: size(8), data :: binary>>, current, stack) do
+      options(data, [], [[255, 253, byte], Enum.reverse(current) | stack])
+    end
+
+    def options(<<255, data :: binary>>, current, stack) do
+      options(data, [255], [Enum.reverse(current) | stack])
+    end
+
+    def options(<<byte :: size(8), data :: binary>>, current, stack) do
+      options(data, [byte | current], stack)
+    end
+
+    def parse_sub_negotiation(data) do
+      {stack, data} = sub_option(data, [])
+      {Enum.reverse(stack), data}
+    end
+
+    def sub_option(<<>>, stack), do: {stack, <<>>}
+
+    def sub_option(<<byte :: size(8), 255, 240, data :: binary>>, stack) do
+      {[240, 255, byte | stack], data}
+    end
+
+    def sub_option(<<byte :: size(8), data :: binary>>, stack) do
+      sub_option(data, [byte | stack])
+    end
+
+    @doc """
+    Parse MSSP subnegotiation options
+    """
+    def parse_mssp(data) do
+      data
+      |> mssp(nil, [])
+      |> Enum.reject(&is_nil/1)
+      |> Enum.into(%{}, fn map ->
+        {to_string(Enum.reverse(map[:name])), to_string(Enum.reverse(map[:value]))}
+      end)
+    end
+
+    def mssp([], current, stack) do
+      [current | stack]
+    end
+
+    def mssp([255 | data], current, stack) do
+      mssp(data, current, stack)
+    end
+
+    def mssp([240 | data], current, stack) do
+      mssp(data, current, stack)
+    end
+
+    def mssp([1 | data], current, stack) do
+      mssp(data, %{type: :name, name: [], value: []}, [current | stack])
+    end
+
+    def mssp([2 | data], current, stack) do
+      mssp(data, Map.put(current, :type, :value), stack)
+    end
+
+    def mssp([byte | data], current, stack) do
+      case current[:type] do
+        :name ->
+          mssp(data, Map.put(current, :name, [byte | current.name]), stack)
+
+        :value ->
+          mssp(data, Map.put(current, :value, [byte | current.value]), stack)
+
+        _ ->
+          mssp(data, current, stack)
+      end
+    end
+  end
+end
---
+++ test/gossip/telnet/client_test.exs
@@ -0,0 +1,49 @@
+defmodule Gossip.Telnet.ClientTest do
+  use ExUnit.Case
+
+  alias Gossip.Telnet.Client
+
+  describe "parsing telnet options" do
+    test "a string" do
+      [] = Client.Options.parse("this is a string")
+    end
+
+    test "a single option" do
+      [[255, 251, 86]] = Client.Options.parse(<<255, 251, 86>>)
+    end
+
+    test "multiple options" do
+      [[255, 251, 70], [255, 251, 201], [255, 251, 91]] = Client.Options.parse(<<255, 251, 70, 255, 251, 201, 255, 251, 91>>)
+    end
+
+    test "parsing sub negotitation" do
+      options = <<255, 250, 1, 255, 240, 85>>
+
+      [[255, 250, 1, 255, 240]] = Client.Options.parse(options)
+    end
+
+    test "sub negotiation options" do
+      options =
+        <<255, 250, 1, 78, 65, 77, 69, 2, 69, 120, 86, 101, 110, 116, 117, 114, 101,
+         32, 77, 85, 68, 1, 80, 76, 65, 89, 69, 82, 83, 2, 48, 1, 85, 80, 84, 73, 77,
+         69, 2, 49, 53, 52, 54, 49, 50, 56, 54, 55, 50, 255, 240, 85>>
+
+      {sub, <<85>>} = Client.Options.parse_sub_negotiation(options)
+
+      assert [255, 250 | _] = sub
+    end
+  end
+
+  describe "parsing MSSP variables" do
+    test "pulls out variable names and values" do
+      options =
+        [255, 250, 1, 78, 65, 77, 69, 2, 69, 120, 86, 101, 110, 116, 117, 114, 101,
+         32, 77, 85, 68, 1, 80, 76, 65, 89, 69, 82, 83, 2, 48, 1, 85, 80, 84, 73, 77,
+         69, 2, 49, 53, 52, 54, 49, 50, 56, 54, 55, 50, 255, 240]
+
+      values = Client.Options.parse_mssp(options)
+
+      assert values["NAME"] == "ExVenture MUD"
+    end
+  end
+end
