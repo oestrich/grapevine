86272f8309673b4dcd5513c682e19e56d6dd25c5
Eric Oestrich <eric@oestrich.org>
2018-12-31 15:06:36 -0500

    Set up open graph tags

--- lib/web/controllers/mssp_controller.ex
+++ lib/web/controllers/mssp_controller.ex
@@ -2,6 +2,11 @@ defmodule Web.MSSPController do
   use Web, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> assign(:title, "MSSP Check - Gossip")
+    |> assign(:open_graph_title, "MSSP Check")
+    |> assign(:open_graph_description, "Check your game's MSSP data with Gossip")
+    |> assign(:open_graph_url, mssp_url(conn, :index))
+    |> render("index.html")
   end
 end
