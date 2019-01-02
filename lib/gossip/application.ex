3b0162d8ada74fa217ead1d5b760d5b6dca8e9e7
Eric Oestrich <eric@oestrich.org>
2019-01-01 23:29:29 -0500

    Move reporting check to runtime

--- lib/gossip/application.ex
+++ lib/gossip/application.ex
@@ -3,8 +3,6 @@ defmodule Gossip.Application do
 
   use Application
 
-  @report_errors Application.get_env(:gossip, :errors)[:report]
-
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -20,7 +18,8 @@ defmodule Gossip.Application do
 
     Metrics.Setup.setup()
 
-    if @report_errors do
+    report_errors = Application.get_env(:gossip, :errors)[:report]
+    if report_errors do
       {:ok, _} = Logger.add_backend(Sentry.LoggerBackend)
     end
 
