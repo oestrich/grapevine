e35eb3ceb2d03605a028b0e71ba15e75c398dfd9
Eric Oestrich <eric@oestrich.org>
2019-01-21 20:06:27 -0500

    Generate a release via docker

--- release.sh
+++ release.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
-rm -rf priv/static/*
-cd assets && node node_modules/brunch/bin/brunch build && cd ..
-MIX_ENV=prod mix phx.digest
-MIX_ENV=prod mix release
-rm -r priv/static/*
-cd assets && node node_modules/brunch/bin/brunch build && cd ..
+SHA=`git rev-parse HEAD`
+COOKIE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+DOCKER_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+mkdir -p tmp/
+
+echo -e "travis_fold:start:docker-build\r"
+docker build --build-arg sha=${SHA} --build-arg cookie=${COOKIE} -f Dockerfile.releaser -t grapevine:releaser .
+echo -e "\ntravis_fold:end:docker-build\r"
+
+docker run -ti --name grapevine_releaser_${DOCKER_UUID} grapevine:releaser /bin/true
+docker cp grapevine_releaser_${DOCKER_UUID}:/app/_build/prod/rel/grapevine/releases/2.3.0/grapevine.tar.gz tmp/
+docker rm grapevine_releaser_${DOCKER_UUID}
