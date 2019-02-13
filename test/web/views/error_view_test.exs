b4764186a1b37643dd7c05625f0892433f9edd11
Eric Oestrich <eric@oestrich.org>
2019-02-12 23:05:21 -0500

    Fix tests

--- test/web/views/error_view_test.exs
+++ test/web/views/error_view_test.exs
@@ -4,8 +4,8 @@ defmodule Web.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(Web.ErrorView, "404.html", []) == "Not Found"
+  test "renders 404.html", %{conn: conn} do
+    assert render_to_string(Web.ErrorView, "404.html", [conn: conn]) =~ ~r/Not Found/
   end
 
   test "renders 500.html" do
