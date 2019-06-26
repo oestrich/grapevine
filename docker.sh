e7f00de1e4ae06513eeb0a44dfafc83ca47c6c08
Eric Oestrich <eric@oestrich.org>
2019-06-26 13:27:32 -0400

    Use a bigger cookie when generating a docker image

--- docker.sh
+++ docker.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 set -e
 
+export COOKIE="CmnosZmULxpjMGGVgiQygVIbOUwPorkZzbstmKZl5nvU3CKRMorZ+CDEXEuTVqVB"
+
 sha=`git rev-parse master`
 
 rm -r tmp/build
@@ -8,8 +10,9 @@ mkdir -p tmp/build
 git archive master | tar x -C tmp/build/
 cd tmp/build
 
-docker build --build-arg cookie=cookie -t oestrich/grapevine:${sha} .
+docker build --build-arg cookie=${COOKIE} -t oestrich/grapevine:${sha} .
 docker push oestrich/grapevine:${sha}
 
 cd apps/telnet
-docker build --build-arg cookie=cookie -t oestrich/grapevine_telnet:${sha} .
+docker build --build-arg cookie=${COOKIE} -t oestrich/grapevine_telnet:${sha} .
+docker push oestrich/grapevine_telnet:${sha}
