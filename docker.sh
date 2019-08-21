29612aa9382428194b4bfa486034aafe78d827e1
Eric Oestrich <eric@oestrich.org>
2019-08-20 21:48:37 -0400

    Update telnet dockerfile

--- docker.sh
+++ docker.sh
@@ -1,8 +1,6 @@
 #/bin/bash
 set -e
 
-export COOKIE="CmnosZmULxpjMGGVgiQygVIbOUwPorkZzbstmKZl5nvU3CKRMorZ+CDEXEuTVqVB"
-
 sha=`git rev-parse master`
 
 rm -r tmp/build
@@ -10,9 +8,9 @@ mkdir -p tmp/build
 git archive master | tar x -C tmp/build/
 cd tmp/build
 
-docker build --build-arg cookie=${COOKIE} -t oestrich/grapevine:${sha} .
+docker build -t oestrich/grapevine:${sha} .
 docker push oestrich/grapevine:${sha}
 
 cd apps/telnet
-docker build --build-arg cookie=${COOKIE} -t oestrich/grapevine_telnet:${sha} .
+docker build -t oestrich/grapevine_telnet:${sha} .
 docker push oestrich/grapevine_telnet:${sha}
