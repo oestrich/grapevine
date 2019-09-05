81ac000a829eced84155767d4d272c30b0a6679b
Eric Oestrich <eric@oestrich.org>
2019-09-04 21:11:45 -0400

    Connect PG in github actions

--- .github/test.exs
+++ .github/test.exs
@@ -3,8 +3,8 @@ import Config
 # Configure your database
 config :grapevine_data, GrapevineData.Repo,
   database: "grapevine_test",
-  hostname: "localhost",
-  username: "grapevine",
-  password: "password",
+  hostname: "postgres",
+  username: "postgres",
+  password: "postgres",
   port: 5433,
   pool: Ecto.Adapters.SQL.Sandbox
