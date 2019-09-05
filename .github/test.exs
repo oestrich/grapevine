45f8139bd08e9011043955c4c817343b380453a8
Eric Oestrich <eric@oestrich.org>
2019-09-04 22:59:58 -0400

    Use the exported docker postgres port for test

--- .github/test.exs
+++ .github/test.exs
@@ -3,8 +3,7 @@ import Config
 # Configure your database
 config :grapevine_data, GrapevineData.Repo,
   database: "grapevine_test",
-  hostname: "postgres",
+  hostname: "localhost",
   username: "postgres",
   password: "postgres",
-  port: 5433,
   pool: Ecto.Adapters.SQL.Sandbox
