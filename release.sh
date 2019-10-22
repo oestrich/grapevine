ebf013f6d22459d89dd07eee16dd7a363ef3df31
Eric Oestrich <eric@oestrich.org>
2019-10-22 11:10:26 -0400

    Remove travis fold lines from release script

--- release.sh
+++ release.sh
@@ -5,9 +5,7 @@ DOCKER_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 mkdir -p tmp/
 
-echo -e "travis_fold:start:docker-build\r"
 docker build -f Dockerfile.releaser -t grapevine:releaser .
-echo -e "\ntravis_fold:end:docker-build\r"
 
 docker run -ti --name grapevine_releaser_${DOCKER_UUID} grapevine:releaser /bin/true
 docker cp grapevine_releaser_${DOCKER_UUID}:/opt/grapevine.tar.gz tmp/
