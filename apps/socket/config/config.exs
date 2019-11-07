2fa406f532548c3c20b9e74de868a7863b8ce0e0
Eric Oestrich <eric@oestrich.org>
2019-11-07 15:37:07 -0600

    Configure ecto repos for socket (for CI)

--- apps/socket/config/config.exs
+++ apps/socket/config/config.exs
@@ -1,7 +1,9 @@
 import Config
 
-config :porcelain, driver: Porcelain.Driver.Basic
+config :grapevine_socket, ecto_repos: [GrapevineData.Repo]
 config :grapevine_socket, :pubsub, start: true
+
+config :porcelain, driver: Porcelain.Driver.Basic
 
 if File.exists?("config/#{Mix.env()}.exs") do
   import_config "#{Mix.env()}.exs"
