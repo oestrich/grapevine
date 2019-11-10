d89258b7567c543f5a4ea41923cc936668bfc3f1
Eric Oestrich <eric@oestrich.org>
2019-11-09 21:45:51 -0600

    Remove the global name from the statistics server

--- apps/telnet/lib/telnet/statistics.ex
+++ apps/telnet/lib/telnet/statistics.ex
@@ -4,10 +4,20 @@ defmodule GrapevineTelnet.Statistics do
   """
 
   def session_started(game, sid) do
-    GenServer.cast({:global, Grapevine.Statistics}, {:start_session, game, sid})
+    GenServer.cast(statistics_pid(), {:start_session, game, sid})
   end
 
   def session_closed(sid) do
-    GenServer.cast({:global, Grapevine.Statistics}, {:close_session, sid})
+    GenServer.cast(statistics_pid(), {:close_session, sid})
+  end
+
+  defp statistics_pid() do
+    case :pg2.get_closest_pid(Grapevine.Statistics) do
+      {:error, reason} ->
+        raise "Could not find closest pid for statistics - #{inspect(reason)}"
+
+      pid ->
+        pid
+    end
   end
 end
