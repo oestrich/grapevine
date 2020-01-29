b1f48612f36ba8039def18dce427693aa42b1437
Eric Oestrich <eric@oestrich.org>
2020-01-29 18:31:52 -0500

    Start ssl with the grapevine_data app

--- apps/data/mix.exs
+++ apps/data/mix.exs
@@ -15,7 +15,7 @@ defmodule GrapevineData.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :ssl],
       mod: {GrapevineData.Application, []}
     ]
   end
