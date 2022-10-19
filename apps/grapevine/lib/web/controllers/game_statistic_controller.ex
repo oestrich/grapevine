97fbb40cba7da8dede232dd6bb530dd63eae1e89
Karol Lassak <ingwar@ingwar.eu.org>
2022-10-19 17:55:05 +0200

    Fix formatting

--- apps/grapevine/lib/web/controllers/game_statistic_controller.ex
+++ apps/grapevine/lib/web/controllers/game_statistic_controller.ex
@@ -37,7 +37,15 @@ defmodule Web.GameStatisticController do
     case Games.get_by_short(short_name) do
       {:ok, game} ->
         conn
-        |> assign(:statistics, Statistics.player_statistics(game, stat_type(params), series_days(params), series_step(params)))
+        |> assign(
+          :statistics,
+          Statistics.player_statistics(
+            game,
+            stat_type(params),
+            series_days(params),
+            series_step(params)
+          )
+        )
         |> put_resp_header("cache-control", "public, max-age=3600")
         |> render("players.json")
 
@@ -55,15 +63,14 @@ defmodule Web.GameStatisticController do
 
   # Values for different data series
   defp series_days(%{"series" => "48-hours"}), do: 2
-  defp series_days(%{"series" => "week"}),     do: 7
-  defp series_days(%{"series" => "month"}),    do: 30
-  defp series_days(%{"series" => "year"}),     do: 360
-  defp series_days(_),                         do: 2
+  defp series_days(%{"series" => "week"}), do: 7
+  defp series_days(%{"series" => "month"}), do: 30
+  defp series_days(%{"series" => "year"}), do: 360
+  defp series_days(_), do: 2
 
   defp series_step(%{"series" => "48-hours"}), do: 1
-  defp series_step(%{"series" => "week"}),     do: 4
-  defp series_step(%{"series" => "month"}),    do: 24
-  defp series_step(%{"series" => "year"}),     do: 168
-  defp series_step(_),                         do: 1
-
+  defp series_step(%{"series" => "week"}), do: 4
+  defp series_step(%{"series" => "month"}), do: 24
+  defp series_step(%{"series" => "year"}), do: 168
+  defp series_step(_), do: 1
 end
